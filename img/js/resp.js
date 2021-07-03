55c5b7dca993603b3b6182046760d49e68d4fa4b
Ayush Negi <86602274+AyushNegi008@users.noreply.github.com>
2021-07-03 10:23:46 +0530

    Update resp.js

--- img/js/resp.js
+++ img/js/resp.js
@@ -12,4 +12,31 @@ burger.addEventListener('click',()=>{
 
 })
 
+pocox = document.querySelector('.pocox')
+section = document.querySelector('.section')
+thumbnil = document.querySelector('.thumbnil')
+para= document.querySelector('.para')
+
+
+
+pocox.addEventListener('click',()=>{
+    para.classList.toggle('v-resp');
+    thumbnil.classList.toggle('v-resp');
+    section.classList.toggle('h-resp');
+
+})
+
+pocoxa = document.querySelector('.pocoxa')
+sectiona = document.querySelector('.sectiona')
+thumbnila = document.querySelector('.thumbnila')
+paraa= document.querySelector('.paraa')
+
+
+
+pocoxa.addEventListener('click',()=>{
+    paraa.classList.toggle('v-a-resp');
+    thumbnila.classList.toggle('v-a-resp');
+    sectiona.classList.toggle('h-a-resp');
+
+})
 
