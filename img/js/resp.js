7113c2731b2bae3651f13d5dceff8b2ce135c2f9
Ayush Negi <86602274+AyushNegi008@users.noreply.github.com>
2021-07-04 15:07:48 +0530

    Update resp.js

--- img/js/resp.js
+++ img/js/resp.js
@@ -31,7 +31,20 @@ sectiona = document.querySelector('.sectiona')
 thumbnila = document.querySelector('.thumbnila')
 paraa= document.querySelector('.paraa')
 
-
+function search_animal() {
+    let input = document.getElementById('searchbar').value
+    input=input.toLowerCase();
+    let x = document.getElementsByClassName('animals');
+      
+    for (i = 0; i < x.length; i++) { 
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display="none";
+        }
+        else {
+            x[i].style.display="list-item";                 
+        }
+    }
+}
 
 pocoxa.addEventListener('click',()=>{
     paraa.classList.toggle('v-a-resp');
